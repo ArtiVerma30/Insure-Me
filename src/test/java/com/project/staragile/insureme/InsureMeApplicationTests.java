14d88660df3603e61f6a6d96f21a1fc2fa87d55f
Arti <avartiverma3001@gmail.com>
2023-10-24 22:54:42 +0530

    added ansible playbook

--- src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
+++ src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
@@ -6,23 +6,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InsureMeApplicationTests {
+public class InsureMeApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 	
 	@Test
-	void testCreatePolicy() {
+	public void testCreatePolicy() {
 		Policy policy = new Policy(1, "Shubham", "Individual" , 10000, "10-Sep-2021", "10-Sep-2022");
 		PolicyService pService = new PolicyService();
 		//Policy outputPolicy = pService.CreatePolicy();
-		assertEquals(policy.getPolicyId(), pService.generateDummyPolicy().getPolicyId());
+	assertEquals(policy.getPolicyId(), pService.generateDummyPolicy().getPolicyId());
 		
 	}
 	
 	@Test
-	void testSearchPolicy() {
+	public void testSearchPolicy() {
 		PolicyService pService = new PolicyService();
 		assertEquals(null,pService.searchPolicy());
 	}
